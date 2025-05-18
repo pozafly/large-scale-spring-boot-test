c128dbed4032be02c50deaaa71c374908ff6dd1a
pozafly <pozafly@gmail.com>
2025-05-18 14:45:43 +0900

    chore: p6spy 전체 패키지에 추가

--- service/article/src/main/java/kuke/board/article/ArticleApplication.java
+++ service/article/src/main/java/kuke/board/article/ArticleApplication.java
@@ -3,7 +3,7 @@ package kuke.board.article;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "kuke.board")
 public class ArticleApplication {
     public static void main(String[] args) {
         SpringApplication.run(ArticleApplication.class, args);
